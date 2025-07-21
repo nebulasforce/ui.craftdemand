eaf1f24fb507712a3c3c39ff2b98b7a223c14f90
xingshanghe <xingshanghe@gmail.com>
2025-07-21 10:45:37 +0800

    fix: 修正登录框大小

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,11 +1,12 @@
 
 import { AuthenticationForm } from '@/components/AuthenticationForm/AuthenticationForm';
+import { Container } from '@mantine/core';
 
 const LoginPage = () => {
   return (
-    <>
+    <Container size={500} my={80}>
       <AuthenticationForm />
-    </>
+    </Container>
   )
 }
 
