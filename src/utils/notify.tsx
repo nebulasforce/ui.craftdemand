0aac0cd0c0c0e7c1588a730a3aeaab5d64fe607f
xingshanghe <xingshanghe@gmail.com>
2025-07-24 10:02:26 +0800

    fix: 优化通知组件

--- src/utils/notify.tsx
+++ src/utils/notify.tsx
@@ -1,12 +1,14 @@
 // utils/notify.tsx
 import { NotificationData, notifications } from '@mantine/notifications';
-import React from 'react';
+import {ReactNode} from 'react';
+import { IconX, IconCheck, IconAlertTriangle, IconInfoCircle, IconBell } from '@tabler/icons-react';
 
 type NotificationType = 'success' | 'error' | 'warning' | 'info' | 'default';
 type NotificationColor = 'green' | 'red' | 'yellow' | 'blue' | 'dark';
 
 
-const notify = (message: React.ReactNode, type: NotificationType = 'success') => {
+const notify = (message: ReactNode, type: NotificationType = 'success') => {
+  // const icon :ReactNode = <I
   const baseConfig: NotificationData = {
     message,
     autoClose: 3000,
@@ -20,11 +22,21 @@ const notify = (message: React.ReactNode, type: NotificationType = 'success') =>
     info: 'blue',
     default: 'dark',
   };
+  const iconMap: Record<NotificationType, ReactNode> = {
+    success: <IconCheck size={18} />,
+    error: <IconX size={18} />,
+    warning: <IconAlertTriangle size={18} />,
+    info: <IconInfoCircle size={18} />,
+    default: <IconBell size={18} />,
+  };
+
+
 
   // 修正类型断言方式
   const config = {
     ...baseConfig,
     color: colorMap[type],
+    icon: iconMap[type],
   };
 
   return notifications.show(config);
