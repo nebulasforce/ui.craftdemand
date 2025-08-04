a00c94f9b84a0d72aad66805b573ee049aa00b3e
xingshanghe <xingshanghe@gmail.com>
2025-08-04 15:02:20 +0800

    fix: 修正404页面

--- src/components/NotFoundImage/NotFoundImage.tsx
+++ src/components/NotFoundImage/NotFoundImage.tsx
@@ -1,4 +1,5 @@
 import { Button, Container, Image, SimpleGrid, Text, Title } from '@mantine/core';
+import Link from 'next/link';
 import image from './image.svg';
 import classes from './NotFoundImage.module.css';
 
@@ -13,9 +14,11 @@ export function NotFoundImage() {
             Page you are trying to open does not exist. You may have mistyped the address, or the
             page has been moved to another URL. If you think this is an error contact support.
           </Text>
-          <Button variant="outline" size="md" mt="xl" className={classes.control}>
-            Get back to home page
-          </Button>
+          <Link href="/" passHref className={classes.link}>
+            <Button variant="outline" size="md" mt="xl" className={classes.control}>
+              Get back to home page
+            </Button>
+          </Link>
         </div>
         <Image src={image.src} className={classes.desktopImage} />
       </SimpleGrid>
