e0dc8c4d576cbfe0f78fb5dd2d52a0db9b5bbc24
xingshanghe <xingshanghe@gmail.com>
2025-09-02 10:05:31 +0800

    fix: 修正图片前缀

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,14 +1,17 @@
+// src/utils/path.ts
+
+import apiConfig from '../../config/api.config';
+
 // 获取图片完整URL
 export const getImageUrl = (relativePath: string | undefined): string => {
-  if (relativePath) {
-    // 如果已经是完整URL则直接返回
-    if (relativePath.startsWith('http://') || relativePath.startsWith('https://')) {
-      return relativePath;
-    }
+  if (!relativePath) {return '';}
 
-    // 拼接基础URL和相对路径
-    const baseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL || '';
-    return `${baseUrl}${relativePath.startsWith('/') ? '' : '/'}${relativePath}`;
+  // 如果已经是完整URL则直接返回
+  if (relativePath.startsWith('http://') || relativePath.startsWith('https://')) {
+    return relativePath;
   }
-  return '';
+
+  // 拼接基础URL和相对路径，处理斜杠问题
+  const baseUrl = apiConfig.baseURL;
+  return `${baseUrl}${relativePath.startsWith('/') ? '' : '/'}${relativePath}`;
 };
