bce376ad28406319b719b2d4ea530dfe243ffe65
xingshanghe <xingshanghe@gmail.com>
2025-12-07 11:06:34 +0800

    feat: 完成首页背景展示

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -4,47 +4,88 @@ import {
   Text,
   Title,
 } from "@mantine/core";
+import { ThreeDMarquee } from "@/components/Marquee3D/Marquee3D";
 
+const images = [
+  "https://assets.aceternity.com/cloudinary_bkp/3d-card.png",
+  "https://assets.aceternity.com/animated-modal.png",
+  "https://assets.aceternity.com/animated-testimonials.webp",
+  "https://assets.aceternity.com/cloudinary_bkp/Tooltip_luwy44.png",
+  "https://assets.aceternity.com/github-globe.png",
+  "https://assets.aceternity.com/glare-card.png",
+  "https://assets.aceternity.com/layout-grid.png",
+  "https://assets.aceternity.com/flip-text.png",
+  "https://assets.aceternity.com/hero-highlight.png",
+  "https://assets.aceternity.com/carousel.webp",
+  "https://assets.aceternity.com/placeholders-and-vanish-input.png",
+  "https://assets.aceternity.com/shooting-stars-and-stars-background.png",
+  "https://assets.aceternity.com/signup-form.png",
+  "https://assets.aceternity.com/cloudinary_bkp/stars_sxle3d.png",
+  "https://assets.aceternity.com/spotlight-new.webp",
+  "https://assets.aceternity.com/cloudinary_bkp/Spotlight_ar5jpr.png",
+  "https://assets.aceternity.com/cloudinary_bkp/Parallax_Scroll_pzlatw_anfkh7.png",
+  "https://assets.aceternity.com/tabs.png",
+  "https://assets.aceternity.com/cloudinary_bkp/Tracing_Beam_npujte.png",
+  "https://assets.aceternity.com/cloudinary_bkp/typewriter-effect.png",
+  "https://assets.aceternity.com/glowing-effect.webp",
+  "https://assets.aceternity.com/hover-border-gradient.png",
+  "https://assets.aceternity.com/cloudinary_bkp/Infinite_Moving_Cards_evhzur.png",
+  "https://assets.aceternity.com/cloudinary_bkp/Lamp_hlq3ln.png",
+  "https://assets.aceternity.com/macbook-scroll.png",
+  "https://assets.aceternity.com/cloudinary_bkp/Meteors_fye3ys.png",
+  "https://assets.aceternity.com/cloudinary_bkp/Moving_Border_yn78lv.png",
+  "https://assets.aceternity.com/multi-step-loader.png",
+  "https://assets.aceternity.com/vortex.png",
+  "https://assets.aceternity.com/wobble-card.png",
+  "https://assets.aceternity.com/world-map.webp",
+];
 
 export default function Home() {
   return (
-    <>
-      <Title className="text-center mt-20">
-        Welcome to{" "}
+    <div className="relative min-h-screen">
+      <ThreeDMarquee
+        className="pointer-events-none fixed inset-0 h-full w-full z-0"
+        images={images}
+      />
+      <div className="fixed inset-0 z-10 h-full w-full bg-white/60 dark:bg-black/60" />
+      <div className="relative z-20">
+        <Title className="text-center mt-20">
+          Welcome to{" "}
+          <Text
+            inherit
+            variant="gradient"
+            component="span"
+            gradient={{ from: "pink", to: "yellow" }}
+          >
+            Mantine
+          </Text>{" "}
+          +
+          <Text
+            inherit
+            variant="gradient"
+            component="span"
+            gradient={{ from: "blue", to: "green" }}
+          >
+            TailwindCSS
+          </Text>
+        </Title>
         <Text
-          inherit
-          variant="gradient"
-          component="span"
-          gradient={{ from: "pink", to: "yellow" }}
+          className="text-center text-gray-700 dark:text-gray-300 max-w-[500px] mx-auto mt-xl"
+          ta="center"
+          size="lg"
+          maw={580}
+          mx="auto"
+          mt="xl"
         >
-          Mantine
-        </Text>{" "}
-        +
-        <Text
-          inherit
-          variant="gradient"
-          component="span"
-          gradient={{ from: "blue", to: "green" }}
-        >
-          TailwindCSS
+          This starter Next.js project includes a minimal setup for Mantine with
+          TailwindCSS. To get started edit page.tsx file.
         </Text>
-      </Title>
-      <Text
-        className="text-center text-gray-700 dark:text-gray-300 max-w-[500px] mx-auto mt-xl"
-        ta="center"
-        size="lg"
-        maw={580}
-        mx="auto"
-        mt="xl"
-      >
-        This starter Next.js project includes a minimal setup for Mantine with
-        TailwindCSS. To get started edit page.tsx file.
-      </Text>
 
-      <div className="flex justify-center mt-10">
-        <ColorSchemeToggle />
+        <div className="flex justify-center mt-10">
+          <ColorSchemeToggle />
+        </div>
+        <Welcome />
       </div>
-      <Welcome />
-    </>
+    </div>
   );
 }
